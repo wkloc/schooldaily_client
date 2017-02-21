2cdbf1818d8cad148e35b535973273452d270be9
Mirosław Małek <mmalek@pgs-soft.com>
2017-02-21 07:19:10 +0100

    csrf protection

--- src/main/java/com/pgs/config/SecurityConfig.java
+++ src/main/java/com/pgs/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 /**
  * Created by mmalek on 2/16/2017.
@@ -16,11 +21,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/**");
     }
 
+    RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+
+        // Always allow the HTTP GET method
+        private Pattern allowedMethods = Pattern.compile("^GET$");
+
+        // Disable CSFR protection on the following urls:
+        private AntPathRequestMatcher[] requestMatchers = {
+                new AntPathRequestMatcher("/login"),
+                new AntPathRequestMatcher("/logout"),
+                new AntPathRequestMatcher("/verify/**")
+        };
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            // Skip allowed methods
+            if (allowedMethods.matcher(request.getMethod()).matches()) {
+                return false;
+            }
+
+            // If the request match one url the CSFR protection will be disabled
+            for (AntPathRequestMatcher rm : requestMatchers) {
+                if (rm.matches(request)) { return false; }
+            }
+
+            return true;
+        } // method matches
+    };
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf().disable()
+                .csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and()
                 .authorizeRequests()
                 .antMatchers("/**").permitAll();
+//                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
