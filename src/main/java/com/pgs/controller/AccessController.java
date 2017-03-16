33568d20063b73b38b24dda71ba6fef1e2d0b01d
Mirosław Małek <mmalek@pgs-soft.com>
2017-03-16 09:01:29 +0100

    add profiles

--- src/main/java/com/pgs/controller/AccessController.java
+++ src/main/java/com/pgs/controller/AccessController.java
@@ -3,6 +3,7 @@ package com.pgs.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,30 +19,33 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class AccessController {
 
+    @Value("${schooldaily.server.url}")
+    private String serverUrl;
+
     @Autowired
     private OAuth2RestOperations restTemplate;
 
     @RequestMapping(value = "/user")
     public JsonNode getProtectedResource() {
-        JsonNode forObject = restTemplate.getForObject("https://localhost:8443/secure/user", JsonNode.class);
+        JsonNode forObject = restTemplate.getForObject(serverUrl + "/secure/user", JsonNode.class);
         return forObject;
     }
 
     @RequestMapping(value = "/user2")
     public JsonNode getProtectedResource2() {
-        JsonNode forObject = restTemplate.getForObject("https://localhost:8443/secure/user2", JsonNode.class);
+        JsonNode forObject = restTemplate.getForObject(serverUrl + "/secure/user2", JsonNode.class);
         return forObject;
     }
 
     @RequestMapping(value = "/kraj", method = RequestMethod.GET)
     @ResponseBody
     public String kraj() {
-        return restTemplate.getForObject("https://localhost:8443/secure/country", String.class);
+        return restTemplate.getForObject(serverUrl + "/secure/country", String.class);
     }
 
     @RequestMapping(value = "/facebook", method = RequestMethod.GET)
     public String facebookString() {
-        return restTemplate.getForObject("https://localhost:8443/secure/facebook", String.class);
+        return restTemplate.getForObject(serverUrl + "/secure/facebook", String.class);
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
@@ -53,7 +57,7 @@ public class AccessController {
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(HttpServletRequest request, HttpServletResponse response) {
 
-        String ret = restTemplate.getForObject("https://localhost:8443/oauth/logout", String.class);
+        String ret = restTemplate.getForObject(serverUrl + "/oauth/logout", String.class);
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
             cookie.setMaxAge(0);
